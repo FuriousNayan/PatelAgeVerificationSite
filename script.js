b462c656042f6b8bb2db2aa0d0134c8110f7b126
Nmpatel133 <nmpatel133@gmail.com>
2025-01-28 17:20:17 -0500

    Pushing Final website for Age Checking

---
+++ script.js
@@ -0,0 +1,26 @@
+const myText = document.getElementById('inputID');
+const mySubmit = document.getElementById('buttonID');
+const myResult = document.getElementById('resultID');
+
+let age;
+
+mySubmit.onclick = function () {
+    age = myText.value;
+    age = Number(age);
+
+    if (age < 18){
+        myResult.textContent = 'You are under the age of 18, therefore you are a minor.'
+    }
+
+    else if (age >= 18 && age <= 30){
+        myResult.textContent = 'You are over the age of 18 but less than 30, therefore you are an adult.'
+    }
+
+    else if (age > 30 && age <= 65){
+        myResult.textContent = 'You are over the age of 30 but less than 65, therefore you are an unc.'
+    }
+
+    else if (age > 65){
+        myResult.textContent = 'You are over the age of 65, therefore you are a senior citizen.'
+    }
+}
